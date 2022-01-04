99becfd4e5707c6de36c1021c8f7e170b4eb3f00
Andrey Shemet <an-shem@ukr.net>
2022-01-04 17:12:57 +0200

    Сделал пагинацию для коллекции контактов и подкорректировал вывод данных о владельце контактов

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,7 +2,12 @@ const { Contact } = require('../../models');
 
 const getAll = async (req, res) => {
   const { _id } = req.user;
-  const contacts = await Contact.find({ owner: _id }).populate('owner', '_id email subscription');
+  const { page = 1, limit = 10 } = req.query;
+  const skip = (page - 1) * limit;
+  const contacts = await Contact.find({ owner: _id }, '', { skip, limit: Number(limit) }).populate(
+    'owner',
+    '_id email subscription'
+  );
   if (!contacts || contacts.length === 0) {
     const error = new Error('Not found');
     error.status = 404;
