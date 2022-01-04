72daf9c2b23530c06245841c0a89dad66d1671df
Andrey Shemet <an-shem@ukr.net>
2022-01-04 19:47:56 +0200

    Сделаk фильтрацию контактов по полю избранного

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,12 +2,24 @@ const { Contact } = require('../../models');
 
 const getAll = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite } = req.query;
+  console.log('favorite', typeof favorite);
   const skip = (page - 1) * limit;
-  const contacts = await Contact.find({ owner: _id }, '', { skip, limit: Number(limit) }).populate(
-    'owner',
-    '_id email subscription'
-  );
+
+  let contacts = null;
+  if (favorite) {
+    console.log('if');
+    contacts = await Contact.find({ owner: _id, favorite }, '', {
+      skip,
+      limit: Number(limit),
+    }).populate('owner', '_id email subscription');
+  } else {
+    console.log('else');
+    contacts = await Contact.find({ owner: _id }, '', {
+      skip,
+      limit: Number(limit),
+    }).populate('owner', '_id email subscription');
+  }
   if (!contacts || contacts.length === 0) {
     const error = new Error('Not found');
     error.status = 404;
