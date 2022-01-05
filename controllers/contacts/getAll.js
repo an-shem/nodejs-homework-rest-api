24b3c5d56eb9c17467c3ef82519f02d337791da8
Andrey Shemet <an-shem@ukr.net>
2022-01-05 22:56:39 +0200

    Удалил проверочные console.log

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,18 +3,15 @@ const { Contact } = require('../../models');
 const getAll = async (req, res) => {
   const { _id } = req.user;
   const { page = 1, limit = 10, favorite } = req.query;
-  console.log('favorite', typeof favorite);
   const skip = (page - 1) * limit;
 
   let contacts = null;
   if (favorite) {
-    console.log('if');
     contacts = await Contact.find({ owner: _id, favorite }, '', {
       skip,
       limit: Number(limit),
     }).populate('owner', '_id email subscription');
   } else {
-    console.log('else');
     contacts = await Contact.find({ owner: _id }, '', {
       skip,
       limit: Number(limit),
