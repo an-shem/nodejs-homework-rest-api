b5769a343612e49adb464c4f9379feeb990453be
Andrey Shemet <an-shem@ukr.net>
2022-01-09 23:28:14 +0200

    Настроил добавление пользователю рандомной аватарки и возможность ее замены на загружаемую пользователем
    - подключил раздачу статических файлов
    - создал  миделвару для получения файлов из запроса
    - сделал добавление avatarURl при регистрации с помощью gravatar
    - сделал маршрут для замены аватара на тот что загружается пользователем

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -1,5 +1,6 @@
 const { Conflict } = require('http-errors');
 const bcrypt = require('bcryptjs');
+const gravatar = require('gravatar');
 const { User } = require('../../models');
 
 const register = async (req, res) => {
@@ -8,8 +9,9 @@ const register = async (req, res) => {
   if (user) {
     throw new Conflict(`Email in use`);
   }
+  const avatarURL = gravatar.url(email);
   const hachPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-  await User.create({ email, password: hachPassword });
+  await User.create({ email, password: hachPassword, avatarURL });
   res.status(201).json({
     user: {
       email,
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const { users: ctrl } = require('../../controllers');
-const { auth, validation, ctrlWrapper } = require('../../middlewares');
+const { auth, upload, validation, ctrlWrapper } = require('../../middlewares');
 const { joiSchemaUser, subscriptionJoiSchema } = require('../../models/user');
 
 router.post('/signup', validation(joiSchemaUser), ctrlWrapper(ctrl.register));
@@ -10,5 +10,6 @@ router.post('/login', validation(joiSchemaUser), ctrlWrapper(ctrl.login));
 router.get('/logout', auth, ctrlWrapper(ctrl.logout));
 router.get('/current', auth, ctrlWrapper(ctrl.current));
 router.patch('/', auth, validation(subscriptionJoiSchema), ctrlWrapper(ctrl.updateSubscription));
+router.patch('/avatars', auth, upload.single('avatar'), ctrlWrapper(ctrl.updateAvatar));
 
 module.exports = router;
